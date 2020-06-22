15aa6a04b32aaa4aae9f8a0c66850f1e28bfc5d5
Shannon <49130897+AdoptedChildOfGod@users.noreply.github.com>
2020-06-22 10:41:44 -0600

    Initial Commit

---
+++ WhereTo/WhereTo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereTo
+//
+//  Created by Shannon Draeker on 6/22/20.
+//  Copyright © 2020 Shannon Draeker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
