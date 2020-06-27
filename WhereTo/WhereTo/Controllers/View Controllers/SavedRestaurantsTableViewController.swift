e6743095bd4b5d7e04912605c702a6b01a37da05
Bryce Bradshaw <brycewbradshaw@icloud.com>
2020-06-27 13:39:17 -0600

    added segementedContrller functionality in FavoritesTableVC

--- WhereTo/WhereTo/Controllers/View Controllers/SavedRestaurantsTableViewController.swift
+++ WhereTo/WhereTo/Controllers/View Controllers/SavedRestaurantsTableViewController.swift
@@ -67,6 +67,7 @@ class SavedRestaurantsTableViewController: UITableViewController {
     }
     
     @IBAction func segmentedControlTapped(_ sender: Any) {
+        self.tableView.reloadData()
     }
     
     // MARK: - Table view data source
@@ -113,11 +114,19 @@ extension SavedRestaurantsTableViewController: RestaurantTableViewCellSavedButto
         guard let currentUser = UserController.shared.currentUser else { return }
         
         if (currentUser.favoriteRestaurants.contains(restaurantID)) {
-            currentUser.favoriteRestaurants.removeAll(where: {$0 == restaurantID})
-            UserController.shared.saveChanges(to: currentUser) { (result) in
+            presentLocationSelectionAlert { (result) in
                 switch result {
                 case .success(_):
-                    cell.isSavedButton.isSelected = false
+                    currentUser.favoriteRestaurants.removeAll(where: {$0 == restaurantID})
+                    UserController.shared.saveChanges(to: currentUser) { (result) in
+                        switch result {
+                        case .success(_):
+                            cell.isSavedButton.isSelected = false
+                        case .failure(let error):
+                            print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                            return
+                        }
+                    }
                 case .failure(let error):
                     print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
                     return
@@ -139,3 +148,25 @@ extension SavedRestaurantsTableViewController: RestaurantTableViewCellSavedButto
     }
 }
 
+
+// MARK: - Extension:SavedRestaurantsTableViewController: Confirm removal from Favs
+extension SavedRestaurantsTableViewController {
+    
+    // Present an alert with a text field to get some input from the user
+    func presentLocationSelectionAlert(completion: @escaping (Result<Bool, WhereToError>) -> Void) {
+        // Create the alert controller
+        let alertController = UIAlertController(title: "Are you sure you want to remove from favorites?", message: "", preferredStyle: .alert)
+        
+        // Create the cancel button
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        // Create the Current Location button
+        let removeFromFavorites = UIAlertAction(title: "Yes", style: .default) { (_) in
+            return completion(.success(true))
+            }
+        // Add the buttons to the alert and present it
+        alertController.addAction(cancelAction)
+        alertController.addAction(removeFromFavorites)
+        present(alertController, animated: true)
+    }
+}
