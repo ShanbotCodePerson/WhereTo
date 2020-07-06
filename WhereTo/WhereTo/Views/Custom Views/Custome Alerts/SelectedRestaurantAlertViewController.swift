f0dab99af5c1f46b555728a4c7a7d65ea0256918
Bryce Bradshaw <brycewbradshaw@icloud.com>
2020-07-06 16:37:57 -0600

    updated restaurantSelectedAlert.

--- WhereTo/WhereTo/Views/Custom Views/Custome Alerts/SelectedRestaurantAlertViewController.swift
+++ WhereTo/WhereTo/Views/Custom Views/Custome Alerts/SelectedRestaurantAlertViewController.swift
@@ -17,7 +17,7 @@ class SelectedRestaurantAlertViewController: UIViewController {
     @IBOutlet weak var alertView: UIView!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet var starRating: [UIImageView]!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var categoriesLabel: UILabel!
     @IBOutlet weak var restaurantImage: UIImageView!
@@ -33,12 +33,14 @@ class SelectedRestaurantAlertViewController: UIViewController {
     
     // MARK: - Actions
     @IBAction func cancelButtonTapped(_ sender: Any) {
-        // TODO: DisMiss View
+        navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion: nil)
     }
     @IBAction func openMapsButtonTapped(_ sender: Any) {
         guard let restaurant = restaurant else { return }
-        // TODO: DisMiss View
         launchMapWith(restaurant: restaurant)
+        navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion: nil)
     }
     
     // MARK: - Helpers
@@ -60,7 +62,16 @@ class SelectedRestaurantAlertViewController: UIViewController {
         messageLabel.text = "Random Picker Chose"
         nameLabel.text = restaurant.name
         addressLabel.text = restaurant.location.displayAddress.joined(separator: ", ")
-        ratingLabel.text = "Rating: \(restaurant.rating ?? 3)"
-        restaurantImage.image = image
+        categoriesLabel.text = restaurant.categoryNames.joined(separator: ", ")
+        starRating.forEach { $0.image = UIImage(systemName: "star") }
+        if let rating = restaurant.rating {
+            let intRating = Int(rating)
+            for index in 0..<intRating {
+                starRating.first(where: { $0.tag == index })?.image = UIImage(systemName: "star.fill")
+            }
+            if rating > Float(intRating) {
+                starRating.first(where: { $0.tag == intRating })?.image = UIImage(systemName: "star.lefthalf.fill")
+            }
+        }
     }
 }
